bb1711c8cf683d29ad116fd90b6ae14817918ceb
Nikita Vorobev <xlopok188@gmail.com>
2019-06-28 11:26:57 +0400

    module3-task1 fixed

--- js/main.js
+++ js/main.js
@@ -3,20 +3,15 @@
 // 1. Создайте массив, состоящий из 8 сгенерированных JS объектов
 
 var housingTypeArray = ['palace', 'flat', 'house', 'bungalo'];
+var pinWidth = 50;
+var pinHeight = 70;
 
 // Получить рандомное число от и до
 function myRandom(from, to) {
   return Math.floor((Math.random() * (to - from + 1)) + from);
 }
 
-// Получить раномный элемент из массива
-var getRandomArrayElement = function (myArray) {
-  var rand = myArray[Math.floor(Math.random() * myArray.length)];
-  return rand;
-};
-
 // Получить рандомное значение Х метки в зависисости от ширины блока
-
 var getMapWidth = function (block) {
   return myRandom(0, block.offsetWidth);
 };
@@ -28,18 +23,18 @@ var mockerDataGenerator = function () {
 
     dataArray[i] = {
       'author': {
-        'avatar': 'img/avatars/user' + '0' + myRandom(1, 8) + '.png'
+        'avatar': 'img/avatars/user' + '0' + (i + 1) + '.png'
         // строка, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
       },
       'offer': {
-        'type': getRandomArrayElement(housingTypeArray)
+        'type': myRandom(0, housingTypeArray.length - 1)
         // строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
       },
 
       'location': {
-        'x': (getMapWidth(map) + 'px'),
+        'x': (getMapWidth(map) + (pinWidth / 2) + 'px'),
         //  случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-        'y': (myRandom(130, 630) + 'px')
+        'y': (myRandom(130, 630) + pinHeight + 'px')
         // случайное число, координата y метки на карте от 130 до 630.
       }
     };
@@ -47,14 +42,10 @@ var mockerDataGenerator = function () {
   return dataArray;
 };
 
-// --------------------------------------------------------------------
-
 // 2. У блока .map уберите класс .map--faded.
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
-// --------------------------------------------------------------------
-
 
 // 3. На основе данных, созданных в первом пункте, создайте DOM-элементы, соответствующие меткам на карте, и заполните их данными из массива. Итоговую разметку метки .map__pin можно взять из шаблона #pin.
 
@@ -74,8 +65,6 @@ var renderLabels = function (label) {
 // Вызываем функцию для создания массива объектов с данными (пока не получаем данные с сервера)
 var mocker = mockerDataGenerator();
 
-// -------------------------------------------------------------------------------------------------------------------------------------------------
-
 // 4. Отрисуйте сгенерированные DOM-элементы в блок .map__pins. Для вставки элементов используйте DocumentFragment.
 
 var labelsEmbeded = map.querySelector('.map__pins');
@@ -86,8 +75,3 @@ for (var i = 0; i < mocker.length; i++) {
 }
 
 labelsEmbeded.appendChild(fragment);
-
-// console.log(renderLabels(mocker[0]));
-// console.log(renderLabels(mocker));
-
-// console.log(mockerDataGenerator());
